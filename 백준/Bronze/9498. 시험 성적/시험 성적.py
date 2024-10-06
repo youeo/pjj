40e80fcbdaf4f45ca77355c7c54e0f17b2fd4195
youeo <yaj5414@gmail.com>
2024-10-06 20:28:59 +0900

    [Bronze V] Title: 시험 성적, Time: 36 ms, Memory: 31120 KB -BaekjoonHub

---
+++ 백준/Bronze/9498. 시험 성적/시험 성적.py
@@ -0,0 +1,14 @@
+x = int(input())
+if x<90:
+    if x<80:
+        if x<70:
+            if x<60:
+                print('F')
+            else:
+                print('D')
+        else:
+            print('C')
+    else:
+        print('B')
+else:
+    print('A')
