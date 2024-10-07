10ab6cb5d165db3fd614ef194cd0a738e8977bd4
youeo <yaj5414@gmail.com>
2024-10-07 16:54:56 +0900

    [Bronze V] Title: A+B - 8, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(void) {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	int n;
+	cin >> n;
+	vector<long long> a(n);
+	vector<long long> b(n);
+	for (int i = 0; i < n; i++) {
+		cin >> a[i] >> b[i];
+	}
+	for (int i = 0; i < n; i++) {
+		cout << "Case #" << i + 1 << ": " << a[i] << " + " << b[i] << " = " << a[i] + b[i] << '\n';
+	}
+}
