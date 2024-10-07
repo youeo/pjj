7a9040d0d1cc7909e7258a94e3b04f6cdfeb6706
youeo <yaj5414@gmail.com>
2024-10-07 16:06:35 +0900

    [Bronze V] Title: A+B - 3, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
+++ 백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -4,13 +4,12 @@ using namespace std;
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int n, a[100], b[100], c[100];
+	int n, a[100], b[100];
 	cin >> n;
 	for(int i=0; i<n; i++) {
 		cin >> a[i] >> b[i];
-		c[i] = a[i] + b[i];
 	}
 	for (int i = 0; i < n; i++) {
-		cout << c[i] << '\n';
+		cout << a[i] + b[i] << '\n';
 	}
 }
