5d1e532452e8be7f7b9bf8031be841d9a1a9ca64
youeo <yaj5414@gmail.com>
2024-10-07 17:30:47 +0900

    [Bronze V] Title: A+B - 5, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
+++ 백준/Bronze/10952. A＋B － 5/A＋B － 5.cc
@@ -1,17 +1,13 @@
 #include <iostream>
-#include <vector>
 using namespace std;
 
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int i=0;
-	vector<int> a(10000);
-	vector<int> b(10000);
+	int i=0, a, b;
 	while (1) {
-		cin >> a[i] >> b[i];
-		if (a[i] == 0 and b[i] == 0) { break; }
-		cout << a[i] + b[i] << "\n";
-		i++;
+		cin >> a >> b;
+		if (a == 0 and b == 0) { break; }
+		cout << a + b << "\n";
 	}
 }
