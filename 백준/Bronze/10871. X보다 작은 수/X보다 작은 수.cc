7b483729aa7b7725804b6e3421a7dd856411ec79
youeo <yaj5414@gmail.com>
2024-10-11 01:06:58 +0900

    [Bronze V] Title: X보다 작은 수, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
+++ 백준/Bronze/10871. X보다 작은 수/X보다 작은 수.cc
@@ -4,14 +4,14 @@ using namespace std;
 int main(void) {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int n, x, a[10000], j=0;
+	int n, x, a[10000];
 	cin >> n >> x;
-	vector<int> c(n);
 	for (int i = 0;i < n;i++) {
 		cin >> a[i];
-		if (a[i] < x) { c[j++] = a[i]; }
 	}
-	for (int i = 0;i < j;i++) {
-		cout << c[i] << " ";
+	for (int i = 0;i < n;i++) {
+		if (a[i] < x) {
+			cout << a[i] << " ";
+		}
 	}
 }
