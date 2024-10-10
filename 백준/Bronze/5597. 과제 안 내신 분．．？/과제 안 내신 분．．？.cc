f45d099a73cf334f5c67a2b5220860c43d557c5e
youeo <yaj5414@gmail.com>
2024-10-11 02:42:51 +0900

    [Bronze III] Title: 과제 안 내신 분..?, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cc
+++ 백준/Bronze/5597. 과제 안 내신 분．．？/과제 안 내신 분．．？.cc
@@ -4,21 +4,16 @@ using namespace std;
 int main() {
 	ios::sync_with_stdio(0);
 	cin.tie(0);
-	int a, c[30], m=30;
-
-	// 출석부 초기값 세팅
-	for (int i = 0;i < m;i++) {
-		c[i] = i+1;
-	}
+	int a, c[30] = {0};
 
 	// 출석 체크
 	for (int i = 0;i < 28;i++) {
 		cin >> a;
-		c[a-1] = 0;
+		c[a-1]++;
 	}
 
 	//미제출자 출력
-	for (int i = 0;i < m;i++) {
-		if (c[i] != 0) { cout << i + 1 << "\n"; }
+	for (int i = 0;i < 30;i++) {
+		if (c[i] == 0) { cout << i + 1 << "\n"; }
 	}
 }
