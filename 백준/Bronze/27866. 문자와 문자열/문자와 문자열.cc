eb368c856e6983508383b4960a5563e3fc7cfcb8
youeo <yaj5414@gmail.com>
2024-10-14 18:21:14 +0900

    [Bronze V] Title: 문자와 문자열, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/27866. 문자와 문자열/문자와 문자열.cc
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+	ios::sync_with_stdio(0);
+	cin.tie(0);
+	string n;
+	int i;
+	cin >> n;
+	cin >> i;
+
+	// i번째 글자 찾아 출력
+	cout << n[i-1];
+}
