d5561c4cd17fb9c9aae188b8467257c662392286
youeo <yaj5414@gmail.com>
2024-10-20 19:36:56 +0900

    [Bronze III] Title: 그대로 출력하기, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Bronze/11718. 그대로 출력하기/그대로 출력하기.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+
+int main() {
+	cin.tie(0)->ios::sync_with_stdio(0);
+	int sum = 0;
+	string a;
+
+	while (100) {
+		getline(cin, a);
+		cout << a << '\n';
+
+		if (a[0] == ' ' or a[0] == NULL) { break; }
+	}
+}
