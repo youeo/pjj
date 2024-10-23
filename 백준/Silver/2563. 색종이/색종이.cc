4c4dc8cda54abda536d2efe33698d7dde6272f64
youeo <yaj5414@gmail.com>
2024-10-24 00:01:35 +0900

    [Silver V] Title: 색종이, Time: 0 ms, Memory: 2060 KB -BaekjoonHub

--- 백준/Silver/2563. 색종이/색종이.cc
+++ 백준/Silver/2563. 색종이/색종이.cc
@@ -9,21 +9,19 @@ int n, x, y;
 
 int main() {
 	cin.tie(0)->ios::sync_with_stdio(0);
+	int sum = 0;
 
 	cin >> n;
-	int sum = n * 100;
 
 	// 색종이 추가
 	for (int i = 0; i < n; i++) {
 		cin >> x >> y;
 		for (int j = x; j < x + 10; j++) {
 			for (int k = y; k < y + 10; k++) {
-				if (arr[j][k] >= 1) {
-					arr[j][k] += 1;
-					sum--;
-				}
-				else {
+				// 겹치는 부분 제외
+				if (arr[j][k] == 0) {
 					arr[j][k] = 1;
+					sum++;
 				}
 			}
 		}
