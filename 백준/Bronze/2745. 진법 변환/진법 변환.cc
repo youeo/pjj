095ac2a69262b554256579cce1d27c5689be1cf2
youeo <yaj5414@gmail.com>
2024-10-24 20:34:09 +0900

    [Bronze II] Title: 진법 변환, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

--- 백준/Bronze/2745. 진법 변환/진법 변환.cc
+++ 백준/Bronze/2745. 진법 변환/진법 변환.cc
@@ -3,7 +3,7 @@
 using namespace std;
 
 //=====================================
-int x;
+int b, x;
 string n;
 //=====================================
 
@@ -12,13 +12,16 @@ int main() {
 	cin.tie(0)->ios::sync_with_stdio(0);
 	int sum = 0;
 
-	cin >> n >> x;
+	cin >> n >> b;
 
+	// 변환
 	for (int i = 0; i < n.size(); i++) {
 		if (n[i] <= '9')
-			sum += (n[i] - 48) * pow(x, n.size() - 1 - i);
+			x = n[i] - 48;
 		else if (n[i] <= 'Z')
-			sum += (n[i] - 55) * pow(x, n.size() - 1 - i);
+			x = n[i] - 55;
+
+		sum += x * pow(b, n.size() - 1 - i);
 	}
 
 	// 결과 출력
