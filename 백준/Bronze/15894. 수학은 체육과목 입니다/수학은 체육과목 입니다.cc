9c3058466c8c828fd73a062d70b334d9b2dc9db4
youeo <yaj5414@gmail.com>
2024-10-26 16:41:48 +0900

    [Bronze III] Title: 수학은 체육과목 입니다, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15894. 수학은 체육과목 입니다/수학은 체육과목 입니다.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//=====================================
+long long n;
+//=====================================
+
+
+int main() {
+	cin.tie(0)->ios::sync_with_stdio(0);
+
+	cin >> n;
+
+	// 결과 출력
+	cout << n * 4;
+}
