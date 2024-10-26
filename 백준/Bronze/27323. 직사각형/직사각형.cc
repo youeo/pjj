cb374898973ad0fdd88f2f12ab8121072f1eaa3d
youeo <yaj5414@gmail.com>
2024-10-26 15:31:58 +0900

    [Bronze V] Title: 직사각형, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/27323. 직사각형/직사각형.cc
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//=====================================
+int a, b;
+//=====================================
+
+
+int main() {
+	cin.tie(0)->ios::sync_with_stdio(0);
+
+	cin >> a >> b;
+
+	cout << a * b;
+}
