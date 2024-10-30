64bc19706aeed0d0f1b88cbf520e73eca1ccee2c
youeo <yaj5414@gmail.com>
2024-10-30 17:22:01 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.cc
@@ -2,10 +2,8 @@
 #include <unordered_map>
 using namespace std;
 
-#define X first
-#define Y second
 //=====================================
-int arr[10], n, i, cnt, k;
+int arr[10], n, i, cnt;
 //=====================================
 
 int MenOfPassion(int A[], int n) {
@@ -17,22 +15,10 @@ int MenOfPassion(int A[], int n) {
 int main() {
 	cin.tie(0)->ios::sync_with_stdio(0);
 	cnt = 0;
-	k = 0;
-
 	cin >> n;
 
 	MenOfPassion(arr, n);
 
-	while (cnt != n && cnt != 1) {
-		if (cnt % n == 0) {
-			cnt /= n;
-			k++;
-		}
-		else if (cnt % n != 0 || k > 3) {
-			k = 4;
-			break;
-		}
-	}
 	cout << cnt << "\n";
-	cout << k;
+	cout << 0;
 }
