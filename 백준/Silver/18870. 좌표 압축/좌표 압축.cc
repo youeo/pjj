aa0f5d4135a804c382cc7b922705a6fc40dd62db
youeo <yaj5414@gmail.com>
2024-11-04 12:53:52 +0900

    [Silver II] Title: 좌표 압축, Time: 456 ms, Memory: 12524 KB -BaekjoonHub

---
+++ 백준/Silver/18870. 좌표 압축/좌표 압축.cc
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//=====================================
+int n, s;
+vector<int> ori, cpy;
+//=====================================
+
+
+int main() {
+	cin.tie(0)->ios::sync_with_stdio(0);
+
+	cin >> n;
+
+	// 좌표 배열 생성
+	for (int i = 0; i < n; i++) {
+		cin >> s;
+		ori.push_back(s);
+		cpy.push_back(s);
+	}
+
+	// 오름차순 정렬 후 중복 제거
+	stable_sort(ori.begin(), ori.end());
+	ori.erase(unique(ori.begin(), ori.end()), ori.end());
+
+	// 좌표의 인덱스 추출 및 결과 출력
+	for (int& e : cpy) {
+		cout << lower_bound(ori.begin(), ori.end(), e) - ori.begin() << ' ';
+	}
+
+	return 0;
+}
