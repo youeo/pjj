3aacb04198c488990f64230dbbe0260bc56d5e39
youeo <yaj5414@gmail.com>
2024-11-05 16:33:00 +0900

    [Silver IV] Title: 숫자 카드 2, Time: 284 ms, Memory: 5224 KB -BaekjoonHub

--- 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
+++ 백준/Silver/10816. 숫자 카드 2/숫자 카드 2.cc
@@ -2,8 +2,8 @@
 using namespace std;
 
 //=====================================
-int n, m, in, up;
-map<int, int> have;
+int n, m, in;
+vector<int> have;
 //=====================================
 
 
@@ -12,20 +12,23 @@ int main() {
 
 	cin >> n;
 
+	// 배열 생성
 	for (int i = 0; i < n; i++) {
 		cin >> in;
 
-		if (have.find(in) != have.end())
-			have[in]++;
-		else
-			have[in] = 1;
+		have.push_back(in);
 	}
+
+	// 정렬
+	sort(have.begin(), have.end());
 
 	cin >> m;
 
+	// 값이 몇 개 있는지 검사 및 출력 (없으면 두 값이 같아서 0이 출력됨)
 	for (int i = 0; i < m; i++) {
 		cin >> in;
-		cout << have[in] << ' ';
+		cout << upper_bound(have.begin(), have.end(), in)
+			- lower_bound(have.begin(), have.end(), in) << ' ';
 	}
 
 	return 0;
