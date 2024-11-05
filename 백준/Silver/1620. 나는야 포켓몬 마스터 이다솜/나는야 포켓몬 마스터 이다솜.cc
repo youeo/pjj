01cb10cba97f74c1445f6824e885e231a0ad6b55
youeo <yaj5414@gmail.com>
2024-11-05 15:59:16 +0900

    [Silver IV] Title: 나는야 포켓몬 마스터 이다솜, Time: 196 ms, Memory: 23944 KB -BaekjoonHub

--- 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
+++ 백준/Silver/1620. 나는야 포켓몬 마스터 이다솜/나는야 포켓몬 마스터 이다솜.cc
@@ -11,15 +11,18 @@ int main() {
 	cin.tie(0)->ios::sync_with_stdio(0);
 
 	cin >> n >> m;
+    
 	map<int, string> idx;
 	map<string, int> poke;
-
+    
+    // 인덱스와 이름 배열을 따로 생성
 	for (int i = 1; i <= n; i++) {
 		cin >> name;
 		idx.insert({ i, name });
 		poke.insert({ name, i });
 	}
 
+    // 인덱스면 이름을, 이름이면 인덱스를 반환 출력
 	for (int i = 0; i < m; i++) {
 		cin >> in;
 		if (isdigit(in[0])) {
