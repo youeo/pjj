504cea76ff7d2eb6476182640d6ea3b632bdccb3
youeo <yaj5414@gmail.com>
2024-11-09 00:01:10 +0900

    [Silver III] Title: 소수 구하기, Time: 12 ms, Memory: 2152 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.cc
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.cc
@@ -2,17 +2,24 @@
 using namespace std;
 
 //=====================================
-int n, m, mx;
+int n, m;
+vector<bool> prime(1000002, true);
 //=====================================
 
-// 소수 확인 함수
-int check(int a) {
-	if (a == 0 || a == 1) { return 0; }
-	if (a != 2 && a % 2 == 0) { return 0; }
-	for (long long i = 3; i * i <= a; i += 2) {
-		if (a % i == 0) { return 0; }
+// 소수 확인 함수 (에라토스테네스의 체)
+static int eratos(vector<bool>& arr) {
+	int s = 1000000;
+
+	arr[0] = 0;
+	arr[1] = 0;
+
+	for (int i = 2; i * i <= s; i++) {
+		if (!prime[i]) { continue; }
+		for (int j = i * i; j <= s; j += i) {
+			prime[j] = 0;
+		}
 	}
-	return 1;
+	return 0;
 }
 
 int main() {
@@ -20,10 +27,11 @@ int main() {
 
 	cin >> n >> m;
 
+	eratos(prime);
+
 	// 결과 출력
 	for (int i = n; i <= m; i++) {
-		mx = i;
-		if (check(mx)) { cout << mx << "\n"; }
+		if (prime[i]) { cout << i << "\n"; }
 	}
 	return 0;
 }
