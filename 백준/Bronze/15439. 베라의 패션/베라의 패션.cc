1e875a49320ee3c53fcc1887411c0c536315da7b
youeo <yaj5414@gmail.com>
2024-11-13 11:09:28 +0900

    [Bronze IV] Title: 베라의 패션, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/15439. 베라의 패션/베라의 패션.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//=====================================
+int n;
+//=====================================
+
+
+int main() {
+	cin.tie(0)->ios::sync_with_stdio(0);
+
+	cin >> n;
+
+	cout << n * (n - 1);
+
+	return 0;
+}
