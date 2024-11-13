5aae9ce612559c6da3c9000ab991b449298edfc7
youeo <yaj5414@gmail.com>
2024-11-13 11:21:12 +0900

    [Bronze IV] Title: 녹색거탑, Time: 0 ms, Memory: 2208 KB -BaekjoonHub

---
+++ 백준/Bronze/24723. 녹색거탑/녹색거탑.cc
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//=====================================
+int n;
+//=====================================
+
+
+int main() {
+	cin.tie(0)->ios::sync_with_stdio(0);
+
+	cin >> n;
+
+	cout << pow(2, n);
+
+	return 0;
+}
