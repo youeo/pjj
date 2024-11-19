9787f9f0c4e6ff2a06d31694d09999620180f8eb
youeo <yaj5414@gmail.com>
2024-11-19 20:15:26 +0900

    [Silver III] Title: N과 M (8), Time: 4 ms, Memory: 2024 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.cc
@@ -0,0 +1,38 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//=====================================
+int n, m, in;
+int arr[10];
+vector<int> num;
+//=====================================
+
+// N과M 수열 재귀 함수(8)
+void nandm(int k, int idx) {
+	if (k == m) {
+		for (int i = 0; i < m; i++)
+			cout << arr[i] << ' ';
+		cout << "\n";
+		return;
+	}
+
+	for (int i = idx; i < n; i++) {
+		arr[k] = num[i];
+		nandm(k + 1, i);
+	}
+}
+
+int main() {
+	cin.tie(0)->ios::sync_with_stdio(0);
+
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		cin >> in;
+		num.push_back(in);
+	}
+	sort(num.begin(), num.end());
+
+	nandm(0, 0);
+
+	return 0;
+}
