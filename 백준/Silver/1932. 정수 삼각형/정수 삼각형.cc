2178b13df4212c70d8ac33700f6685d5793bb830
youeo <yaj5414@gmail.com>
2024-11-26 01:10:47 +0900

    [Silver I] Title: 정수 삼각형, Time: 8 ms, Memory: 3980 KB -BaekjoonHub

---
+++ 백준/Silver/1932. 정수 삼각형/정수 삼각형.cc
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//=====================================
+int n;
+long long mmax = 0;
+long long arr[501][501];
+//=====================================
+
+int main() {
+	cin.tie(0)->ios::sync_with_stdio(0);
+
+	cin >> n;
+
+	// 삼각형 배열 생성
+	for (int i = 1; i <= n; i++) {
+		for (int j = 1; j <= i; j++) {
+			cin >> arr[i][j];
+		}
+	}
+
+	// 각 경로별 값 저장
+	for (int i = 2; i <= n; i++) {
+		for (int j = 1; j <= i; j++) {
+			arr[i][j] = max(arr[i - 1][j - 1] + arr[i][j], arr[i - 1][j] + arr[i][j]);
+		}
+	}
+
+	// 결과 출력
+	cout << *max_element(arr[n], arr[n] + n + 1);
+
+	return 0;
+}
