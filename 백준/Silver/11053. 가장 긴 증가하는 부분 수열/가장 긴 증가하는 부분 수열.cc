2eedbeaa0992da4d8bc70116085ffe666206d952
youeo <yaj5414@gmail.com>
2024-11-26 19:29:52 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.cc
@@ -2,8 +2,8 @@
 using namespace std;
 
 //=====================================
-int n, t;
-int arr[1000], ck[1000];
+int n, in;
+vector<int> arr;
 //=====================================
 
 int main() {
@@ -11,27 +11,19 @@ int main() {
 
 	cin >> n;
 
-	// 수열 배열 생성
 	for (int i = 0; i < n; i++) {
-		cin >> arr[i];
-	}
+		cin >> in;
 
-	// 초기값
-	ck[0] = 1;
-
-	// 가장 긴 증가하는 부분 수열 구하기
-	for (int i = 1; i < n; i++) {
-		t = 0;
-		for (int j = 0; j <= i; j++) {
-			if (arr[i] > arr[j])
-				t = max(t, ck[j]);
+		// 이전 값보다 작거나 같으면 해당 수보다 크거나 같은 수를 찾아 대체하기
+		if (i != 0 && in <= arr.back()) {
+			*(lower_bound(arr.begin(), arr.end(), in)) = in;
+			continue;
 		}
-		if (t == 0) ck[i] = 1;
-		else ck[i] = t + 1;
+		arr.push_back(in);
 	}
 
 	// 결과 출력
-	cout << *max_element(ck, ck + n);
+	cout << arr.size();
 
 	return 0;
 }
