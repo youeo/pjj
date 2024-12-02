29a16747e42d389f62eea2df137b4d907c430364
youeo <yaj5414@gmail.com>
2024-12-02 20:57:26 +0900

    [Silver II] Title: 알고리즘 수업 - 너비 우선 탐색 1, Time: 84 ms, Memory: 9940 KB -BaekjoonHub

---
+++ 백준/Silver/24444. 알고리즘 수업 － 너비 우선 탐색 1/알고리즘 수업 － 너비 우선 탐색 1.cc
@@ -0,0 +1,52 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//=====================================
+int n, m, r, in_m, in_r;
+int vis[100001], ck[100001];
+vector<int> arr[100001];
+//=====================================
+
+int main() {
+	cin.tie(0)->ios::sync_with_stdio(0);
+	int j = 1;
+
+	cin >> n >> m >> r;
+
+	// 너비 우선 탐색 - queue 사용
+	queue<int> q;
+	vis[r] = 1;
+	ck[r] = j;
+	q.push(r);
+
+	// 양방향 간선 구현
+	for (int i = 0; i < m; i++) {
+		cin >> in_m >> in_r;
+		arr[in_m].push_back(in_r);
+		arr[in_r].push_back(in_m);
+	}
+
+	//오름차순 탐색을 위한 정렬
+	for (int i = 1; i <= n; i++) {
+		sort(arr[i].begin(), arr[i].end(), less<>());
+	}
+
+
+	while (!q.empty()) {
+		int cur = q.front(); q.pop();
+		ck[cur] = j++;
+
+		for (int& e : arr[cur]) {
+			if (vis[e]) continue;
+			vis[e] = 1;
+			q.push(e);
+		}
+	}
+
+	// 결과 출력
+	for (int i = 1; i <= n; i++) {
+		cout << ck[i] << "\n";
+	}
+
+	return 0;
+}
