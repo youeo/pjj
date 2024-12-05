35370ee34947e51a64b64030a1092912d530f0b6
youeo <yaj5414@gmail.com>
2024-12-05 18:14:16 +0900

    [Gold V] Title: 토마토, Time: 80 ms, Memory: 14112 KB -BaekjoonHub

---
+++ 백준/Gold/7576. 토마토/토마토.cc
@@ -0,0 +1,70 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//=====================================
+int m, n, mx = 0;
+int arr[1000][1000];
+int vis[1000][1000];
+int dx[4] = { 1, 0, -1, 0 };
+int dy[4] = { 0, 1, 0, -1 };
+queue<pair<int, int>> q;
+//=====================================
+
+void bfs() {
+
+	// 너비 우선 탐색 - queue 사용
+
+	while (!q.empty()) {
+		auto [x, y] = q.front(); q.pop();
+
+		for (int dir = 0; dir < 4; dir++) {
+			int nx = x + dx[dir];
+			int ny = y + dy[dir];
+
+			if (nx < 0 || nx >= n || ny < 0 || ny >= m) continue;
+			if (arr[nx][ny] != 0) continue;
+
+			vis[nx][ny] = vis[x][y] + 1;
+			arr[nx][ny] = 1;
+			q.push({ nx, ny });
+		}
+	}
+}
+
+
+int main() {
+	cin.tie(0)->ios::sync_with_stdio(0);
+
+	cin >> m >> n;
+
+	// 토마토 상자 채우기 & 익은 토마토 확인
+	// 시간 단축을 위해 익은 토마토 한 번에 큐에 삽입
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			cin >> arr[i][j];
+
+			if (arr[i][j] == 1) {
+				vis[i][j] = 1;
+				q.push({ i, j });
+			}
+		}
+	}
+
+	bfs();
+
+	// 최소 날짜 찾기 (익지 못한 토마토 확인)
+	for (int i = 0; i < n; i++) {
+		for (int j = 0; j < m; j++) {
+			if (arr[i][j] == 0) {
+				cout << -1;
+				return 0;
+			}
+			mx = max(mx, vis[i][j]);
+		}
+	}
+
+	// 결과 출력
+	cout << mx - 1;
+
+	return 0;
+}
