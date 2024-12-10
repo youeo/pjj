80902cdd88002e4fd3b1675e848cfeeb66e33f0d
youeo <yaj5414@gmail.com>
2024-12-10 23:00:31 +0900

    [Silver III] Title: 주유소, Time: 24 ms, Memory: 3584 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.cc
+++ 백준/Silver/13305. 주유소/주유소.cc
@@ -4,52 +4,28 @@ using ll = long long;
 
 //=====================================
 int n;
-ll sum_t = 0, cal_n = 0, result = 0, ss = 0;
-ll dis[100001];
-vector<pair<ll, ll>>price;
+ll result = 0, min_p = 1000000001;
+ll dis[100001], price[100001];
 //=====================================
-
-int cmp(pair <ll, ll>& a, pair <ll, ll>& b) {
-	if (a.first == b.first)
-		return a.second > b.second;
-	return a.first < b.first;
-}
 
 int main() {
 	cin.tie(0)->ios::sync_with_stdio(0);
 
 	cin >> n;
-	price.resize(n);
 
 	for (int i = 1; i < n; i++) {
 		cin >> dis[i];
-		sum_t += dis[i];
 	}
-	for (int i = 0; i < n; i++) {
-		cin >> price[i].first;
-	}
-
-	int cc = sum_t;
-	for (int i = 0; i < n; i++) {
-		price[i].second = cc;
-		cc -= dis[i + 1];
+	for (int i = 1; i <= n; i++) {
+		cin >> price[i];
 	}
 
-
-	auto idx = min_element(price.begin(), price.end() - 1, cmp);
-	auto now = *idx;
-
-	result = now.first * now.second;
-	ss = now.second;
-
-	while (ss != sum_t) {
-		idx = min_element(price.begin(), idx, cmp);
-		now = *idx;
-
-		result += now.first * (now.second - ss);
-		ss += (now.second - ss);
+	// 이전 주유소의 가격이 더 낮으면 현재 주유소가 아닌 이전 주유소에서 주유
+	for (int i = 1; i <= n; i++) {
+		if (price[i] < min_p)
+			min_p = price[i];
+		result += min_p * dis[i];
 	}
-
 
 	// 결과 출력
 	cout << fixed << result;
