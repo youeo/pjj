b122b82fdf0220b20af6b47f726f3fb27b3fb26a
youeo <yaj5414@gmail.com>
2024-12-11 21:09:41 +0900

    [Silver I] Title: 곱셈, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

--- 백준/Silver/1629. 곱셈/곱셈.cc
+++ 백준/Silver/1629. 곱셈/곱셈.cc
@@ -9,12 +9,14 @@ ll a, b, c;
 // 분할 곱셈 (재귀 사용)
 ll solve(ll n) {
 
-	if (n == 1) return a;
+	if (n == 1) return a % c;
 
 	ll nn = solve(n / 2);
+
 	// n이 홀수일때와 짝수일때로 구분
+	// 홀수일 때는 2k+1가 되어야 하는데 이것은 k * k * a로 만들 수 있음 (짝수면 k * k = 2k)
 	if (n % 2)
-		return (nn * solve(n - (n / 2))) % c;
+		return (((nn * nn) % c) * a) % c;
 	else
 		return (nn * nn) % c;
 }
@@ -25,7 +27,7 @@ int main() {
 	cin >> a >> b >> c;
 
 	// 결과 출력
-	cout << solve(b) % c;
+	cout << solve(b);
 
 	return 0;
 }
