b161f9eb2a9a803b9333312d6dfb081bfc32f572
youeo <yaj5414@gmail.com>
2024-12-26 17:10:23 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 1056 ms, Memory: 27524 KB -BaekjoonHub

---
+++ 백준/Silver/18352. 특정 거리의 도시 찾기/특정 거리의 도시 찾기.cc
@@ -0,0 +1,56 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//=====================================
+const int INF = 1e9 + 10;
+int n, m, k, x, v1, v2;
+bool f = true;
+int d[300005];
+vector<int> arr[300005];
+//=====================================
+
+// 다익스트라 구현 함수
+void dij(int k) {
+	priority_queue<int, vector<int>, greater<int>> pq;
+
+	d[k] = 0;
+	pq.push(k);
+
+	while (!pq.empty()) {
+		int x = pq.top(); pq.pop();
+
+		for (auto nxt : arr[x]) {
+			if (d[x] != INF && d[nxt] <= d[x] + 1) continue;
+
+			d[nxt] = d[x] + 1;
+			pq.push(nxt);
+		}
+	}
+}
+
+int main() {
+	cin.tie(0)->ios::sync_with_stdio(0);
+
+	cin >> n >> m >> k >> x;
+
+	while (m--) {
+		cin >> v1 >> v2;
+		arr[v1].push_back(v2);
+	}
+
+	fill(d, d + n + 1, INF);
+
+	dij(x);
+
+	for (int i = 1; i <= n; i++) {
+		if (d[i] == k) {
+			cout << i << "\n";
+			f = false;
+		}
+	}
+
+	if (f == true)
+		cout << -1;
+
+	return 0;
+}
