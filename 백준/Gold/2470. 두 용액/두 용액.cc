bc9e19a65c2266d7bd9c23c63232f8bb29b9f121
youeo <yaj5414@gmail.com>
2024-12-31 19:58:18 +0900

    [Gold V] Title: 두 용액, Time: 12 ms, Memory: 4528 KB -BaekjoonHub

---
+++ 백준/Gold/2470. 두 용액/두 용액.cc
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+
+//=====================================
+int n, x;
+ll arr[100'000];
+stack<pair<ll, ll>> ans;
+//=====================================
+
+
+int main() {
+	cin.tie(0)->ios::sync_with_stdio(0);
+	ll min = 0x7f7f7f7f7f7f;
+
+	cin >> n;
+
+	// 배열 생성
+	for (int i = 0; i < n; i++) {
+		cin >> arr[i];
+	}
+
+	// 정렬 필수
+	sort(arr, arr + n);
+
+	// 투 포인터
+	int st = 0, en = n - 1;
+
+	// 각 조건에 따라 포인터 이동
+	while (st != en) {
+		ll sum = arr[st] + arr[en];
+
+		if (abs(sum) <= abs(min)) {
+			min = sum;
+			ans.push({ arr[st], arr[en] });
+		}
+
+		// 더한 값이 0보다 작으면 큰 값으로 이동해야 함
+		// 더한 값이 0보다 크면 작은 값으로 이동해야 함
+		if (sum < 0)
+			st++;
+		else
+			en--;
+	}
+
+	// 결과 출력
+	cout << ans.top().first << ' ' << ans.top().second;
+
+	return 0;
+}
